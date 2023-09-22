695fa06a5d52e5d27fbb414e27b63e61e2f0e7d7
PTThanh1101 <phamtuanthanh11012003@gmail.com>
2023-09-22 19:27:59 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ToMau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.pictureBoxA = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            this.pictureBoxE = new System.Windows.Forms.PictureBox();
+            this.pictureBoxF = new System.Windows.Forms.PictureBox();
+            this.pictureBoxD = new System.Windows.Forms.PictureBox();
+            this.pictureBoxG = new System.Windows.Forms.PictureBox();
+            this.pictureBoxZ = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(690, 357);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(80, 54);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click_1);
+            // 
+            // pictureBoxA
+            // 
+            this.pictureBoxA.Location = new System.Drawing.Point(12, 219);
+            this.pictureBoxA.Name = "pictureBoxA";
+            this.pictureBoxA.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxA.TabIndex = 1;
+            this.pictureBoxA.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(141, 101);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxB.TabIndex = 2;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxC
+            // 
+            this.pictureBoxC.Location = new System.Drawing.Point(141, 219);
+            this.pictureBoxC.Name = "pictureBoxC";
+            this.pictureBoxC.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxC.TabIndex = 3;
+            this.pictureBoxC.TabStop = false;
+            // 
+            // pictureBoxE
+            // 
+            this.pictureBoxE.Location = new System.Drawing.Point(302, 101);
+            this.pictureBoxE.Name = "pictureBoxE";
+            this.pictureBoxE.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxE.TabIndex = 4;
+            this.pictureBoxE.TabStop = false;
+            // 
+            // pictureBoxF
+            // 
+            this.pictureBoxF.Location = new System.Drawing.Point(302, 219);
+            this.pictureBoxF.Name = "pictureBoxF";
+            this.pictureBoxF.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxF.TabIndex = 5;
+            this.pictureBoxF.TabStop = false;
+            // 
+            // pictureBoxD
+            // 
+            this.pictureBoxD.Location = new System.Drawing.Point(141, 337);
+            this.pictureBoxD.Name = "pictureBoxD";
+            this.pictureBoxD.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxD.TabIndex = 6;
+            this.pictureBoxD.TabStop = false;
+            // 
+            // pictureBoxG
+            // 
+            this.pictureBoxG.Location = new System.Drawing.Point(302, 337);
+            this.pictureBoxG.Name = "pictureBoxG";
+            this.pictureBoxG.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxG.TabIndex = 7;
+            this.pictureBoxG.TabStop = false;
+            // 
+            // pictureBoxZ
+            // 
+            this.pictureBoxZ.Location = new System.Drawing.Point(460, 219);
+            this.pictureBoxZ.Name = "pictureBoxZ";
+            this.pictureBoxZ.Size = new System.Drawing.Size(30, 32);
+            this.pictureBoxZ.TabIndex = 8;
+            this.pictureBoxZ.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxZ);
+            this.Controls.Add(this.pictureBoxG);
+            this.Controls.Add(this.pictureBoxD);
+            this.Controls.Add(this.pictureBoxF);
+            this.Controls.Add(this.pictureBoxE);
+            this.Controls.Add(this.pictureBoxC);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBoxA);
+            this.Controls.Add(this.ColorButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxZ)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.PictureBox pictureBoxA;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxC;
+        private System.Windows.Forms.PictureBox pictureBoxE;
+        private System.Windows.Forms.PictureBox pictureBoxF;
+        private System.Windows.Forms.PictureBox pictureBoxD;
+        private System.Windows.Forms.PictureBox pictureBoxG;
+        private System.Windows.Forms.PictureBox pictureBoxZ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ToMau
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<char, Color> vertexColors = new Dictionary<char, Color>();
+        private Dictionary<char, Point> vertexPositions = new Dictionary<char, Point>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGraph();
+            Paint += Form1_Paint;
+            ColorButton.Click += ColorButton_Click_1;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            foreach (char vertex in vertexPositions.Keys)
+            {
+                Point position = vertexPositions[vertex];
+                Rectangle rect = new Rectangle(position.X, position.Y, 30, 30);
+                Color color = vertexColors[vertex];
+                using (Brush brush = new SolidBrush(color))
+                {
+                    g.FillEllipse(brush, rect);
+                }
+                g.DrawEllipse(Pens.Black, rect);
+                g.DrawString(vertex.ToString(), new Font("Arial", 12), Brushes.Black, position.X + 10, position.Y + 10);
+            }
+
+            ConnectAllVertices(g);
+        }
+
+        private void InitializeGraph()
+        {
+            vertexColors['A'] = Color.Black; // Màu ban đầu là đen cho tất cả các đỉnh
+            vertexColors['B'] = Color.Black;
+            vertexColors['C'] = Color.Black;
+            vertexColors['D'] = Color.Black;
+            vertexColors['E'] = Color.Black;
+            vertexColors['F'] = Color.Black;
+            vertexColors['G'] = Color.Black;
+            vertexColors['Z'] = Color.Black;
+
+            // Khởi tạo vị trí các đỉnh
+            vertexPositions['A'] = new Point(12, 219);
+            vertexPositions['B'] = new Point(141, 101);
+            vertexPositions['C'] = new Point(141, 219);
+            vertexPositions['D'] = new Point(141, 337);
+            vertexPositions['E'] = new Point(302, 101);
+            vertexPositions['F'] = new Point(302, 219);
+            vertexPositions['G'] = new Point(302, 337);
+            vertexPositions['Z'] = new Point(460, 219);
+        }
+
+        private void ConnectAllVertices(Graphics g)
+        {
+            // Khai báo các đỉnh kề của mỗi đỉnh
+            Dictionary<char, List<char>> adjacentVertices = new Dictionary<char, List<char>>
+            {
+                { 'A', new List<char> { 'B', 'C', 'D' } },
+                { 'B', new List<char> { 'A', 'C', 'E' } },
+                { 'C', new List<char> { 'A', 'B', 'D', 'E', 'F' } },
+                { 'D', new List<char> { 'A', 'C', 'F', 'G' } },
+                { 'E', new List<char> { 'B', 'C', 'F', 'Z' } },
+                { 'F', new List<char> { 'C', 'D', 'E', 'G', 'Z' } },
+                { 'G', new List<char> { 'D', 'F', 'Z' } },
+                { 'Z', new List<char> { 'E', 'F', 'G' } }
+            };
+
+            // Vẽ các đoạn đường nối giữa các đỉnh
+            foreach (char vertex in vertexPositions.Keys)
+            {
+                Point startPoint = vertexPositions[vertex];
+
+                foreach (char adjacentVertex in adjacentVertices[vertex])
+                {
+                    Point endPoint = vertexPositions[adjacentVertex];
+                    g.DrawLine(Pens.Black, startPoint.X + 15, startPoint.Y + 15, endPoint.X + 15, endPoint.Y + 15);
+                }
+            }
+        }
+
+        public Color GetAvailableColor(char vertex)
+        {
+            HashSet<Color> neighborColors = new HashSet<Color>();
+            foreach (char neighbor in vertexColors.Keys)
+            {
+                if (AreVerticesConnected(vertex, neighbor))
+                {
+                    neighborColors.Add(vertexColors[neighbor]);
+                }
+            }
+
+            List<Color> availableColors = new List<Color>
+    {
+        Color.Red,
+        Color.Green,
+        Color.Blue,
+        Color.Yellow,
+        Color.Orange,
+        Color.Purple
+    };
+
+            // Loại bỏ các màu đã được sử dụng bởi các đỉnh kề
+            foreach (Color usedColor in neighborColors)
+            {
+                availableColors.Remove(usedColor);
+            }
+
+            // Nếu không còn màu nào trống, trả về màu đen
+            if (availableColors.Count == 0)
+            {
+                return Color.Black;
+            }
+
+            // Chọn màu trống đầu tiên trong danh sách
+            return availableColors[0];
+        }
+
+
+        private bool AreVerticesConnected(char vertex1, char vertex2)
+        {
+            // Viết mã để kiểm tra xem hai đỉnh có nối với nhau không, dựa trên yêu cầu của đồ thị
+
+            if ((vertex1 == 'A' && (vertex2 == 'B' || vertex2 == 'C' || vertex2 == 'D')) ||
+                (vertex1 == 'B' && (vertex2 == 'A' || vertex2 == 'C' || vertex2 == 'E')) ||
+                (vertex1 == 'C' && (vertex2 == 'A' || vertex2 == 'B' || vertex2 == 'D' || vertex2 == 'E' || vertex2 == 'F')) ||
+                (vertex1 == 'D' && (vertex2 == 'A' || vertex2 == 'C' || vertex2 == 'F' || vertex2 == 'G')) ||
+                (vertex1 == 'E' && (vertex2 == 'B' || vertex2 == 'C' || vertex2 == 'F' || vertex2 == 'Z')) ||
+                (vertex1 == 'F' && (vertex2 == 'C' || vertex2 == 'D' || vertex2 == 'E' || vertex2 == 'G' || vertex2 == 'Z')) ||
+                (vertex1 == 'G' && (vertex2 == 'D' || vertex2 == 'F' || vertex2 == 'Z')) ||
+                (vertex1 == 'Z' && (vertex2 == 'E' || vertex2 == 'F' || vertex2 == 'G')))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ColorButton_Click_1(object sender, EventArgs e)
+        {
+            List<char> verticesToUpdate = new List<char>();
+
+            foreach (char vertex in vertexColors.Keys)
+            {
+                Color color = GetAvailableColor(vertex);
+
+                // Thêm các đỉnh cần cập nhật màu vào danh sách tạm thời
+                verticesToUpdate.Add(vertex);
+            }
+
+            // Duyệt qua danh sách các đỉnh và cập nhật màu sắc
+            foreach (char vertex in verticesToUpdate)
+            {
+                Color color = GetAvailableColor(vertex);
+                vertexColors[vertex] = color;
+            }
+
+            // Vẽ lại đồ thị sau khi đã tô màu
+            this.Invalidate();
+        }
+    }
+}
